2b6ba1e444fb28ded478e67cacad6b7aa49799df
Hridayansh018 <hridayansh018@gmail.com>
2025-01-13 23:37:50 +0530

    pushing the files

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,77 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '../public/hilink-logo.svg';
+import { FOOTER_LINKS } from '@/constants';
 
 const Footer = () => {
   return (
-    <div>
-      footerS
-    </div>
-  )
-}
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full flex-col gap-14">
+        {/* Top Section */}
+        <div className="flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between w-full gap-10 md:gap-[10%]">
+          {/* Logo */}
+          <Link href="/" className="flex justify-center">
+            <Image src={logo} alt="logo" width={74} height={29} />
+          </Link>
 
-export default Footer
+          {/* Footer Links */}
+          <div className="flex flex-wrap gap-10 justify-center md:justify-between md:flex-1">
+            {FOOTER_LINKS.map((columns) => (
+              <FooterColumn title={columns.title} key={columns.title}>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {columns.links.map((link) => (
+                    <Link href="/" key={link}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            ))}
+
+            {/* Contact Us Section */}
+            <div className="flex flex-col gap-5 items-center md:items-start">
+              <h4 className="bold-18 whitespace-nowrap">Contact Us</h4>
+              <div className="regular-14 flex flex-col gap-4 text-gray-30 text-center md:text-left">
+                <p>Email: <a href="hilinik@travels.com" className="text-primary">hilinik@travels.com</a></p>
+                <p>Phone: <a href="tel:+1234567890" className="text-primary">+1 234 567 890</a></p>
+                <p>Address: <span className="text-gray-40">Jakarta , Indonasia</span></p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Follow Us Section */}
+        <div className="flex flex-col items-center gap-5">
+          <h4 className="bold-18 whitespace-nowrap">Follow Us</h4>
+          <div className="flex gap-4">
+            <Link href="/">
+              <Image src="/facebook.svg" alt="facebook" width={24} height={24} />
+            </Link>
+            <Link href="/">
+              <Image src="/twitter.svg" alt="twitter" width={24} height={24} />
+            </Link>
+            <Link href="/">
+              <Image src="/instagram.svg" alt="instagram" width={24} height={24} />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-5 items-center md:items-start">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  );
+};
--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,10 +1,39 @@
+import Image from "next/image";
+import Button from "./Button";
 
 const GetApp = () => {
   return (
-    <div>
-      GetApp
-    </div>
-  )
-}
+    <section className="flexCenter w-full flex-col pb-[70px]">
+      <div className="get-app flex flex-col lg:flex-row items-center lg:items-start justify-between w-full lg:gap-12">
+        {/* Content Section */}
+        <div className="z-20 flex flex-col items-start justify-center gap-6 lg:flex-1">
+          <h2 className="bold-40 lg:bold-64">Get for free now!</h2>
+          <p className="regular-16 text-gray-10">Available on iOS and Android</p>
+          <div className="flex w-full flex-col gap-3 whitespace-nowrap lg:flex-row">
+            <Button
+              title="App Store"
+              type="button"
+              icon="/apple.svg"
+              variant="btn_white"
+              full={true}
+            />
+            <Button
+              title="Play Store"
+              type="button"
+              icon="/android.svg"
+              variant="btn_dark_green"
+              full={true}
+            />
+          </div>
+        </div>
 
-export default GetApp
+        {/* Image Section */}
+        <div className="flex justify-center lg:flex-1 lg:min-h-[100px]">
+          <p className="bold-40 lg:bold-64 text-green-300">Download The App for Better Experience</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GetApp;
